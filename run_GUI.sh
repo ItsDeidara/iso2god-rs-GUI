9275fd2edefb6ee52afa47ed6c477ed4651f8e92
pepeshki (screamer) <75133653+Pepeshki@users.noreply.github.com>
2025-05-10 00:41:03 +0300

    Fix the script so it runs on linux without errors
    Fixes:
    fix the SCRIPT_DIR variable so it properly goes to the folder
    replace GUI.py with gui.py, because of case-sensitivity

--- run_GUI.sh
+++ run_GUI.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Run the GUI application (GUI.py) using python3 if available, else fallback to python
-SCRIPT_DIR="$(dirname "$0")"
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 if command -v python3 >/dev/null 2>&1; then
-    python3 "$SCRIPT_DIR/GUI.py"
+    python3 "$SCRIPT_DIR/gui.py"
 else
-    python "$SCRIPT_DIR/GUI.py"
+    python "$SCRIPT_DIR/gui.py"
 fi 
